ea899e69cec0de7bb7b7b069add5ffaf015ecaf5
Minimix555 <mix2548@gmail.com>
2025-07-24 10:10:16 +0700

    Add files via upload

---
+++ app/routes/Mix_Lab02.tsx
@@ -0,0 +1,14 @@
+function MyProfile () {
+    return (
+		<div>
+			<p className="text-2xl">Mix Profile</p>
+			<img src="/image/myprofile.png"/>
+			<p className="text - Ig">
+				Name: Thanin Panjai<br />
+				Student ID: 026730491010-7<br />
+			</p>
+		</div>
+	);
+}
+
+export default MyProfile 
